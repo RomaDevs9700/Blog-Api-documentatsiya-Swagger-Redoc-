fed45e7ba2d78e7d7bbf04d0507816cbb333b8e9
Ramazon <94096856+RomaDevs9700@users.noreply.github.com>
2022-06-28 14:15:42 +0500

    Update settings.py

--- RestApi/settings.py
+++ RestApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+h8$=e^r0iv3#7kd!))343*r)os8a&omby%sr&bea-53w&zxs='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
